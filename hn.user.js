e5d371de74e0ba6782f11aa73def5a02587a88ea
Ralph Saunders <e@ralphsaunders.co.uk>
2012-05-14 16:00:26 +0100

    Workflow setup + base

--- hn.user.js
+++ hn.user.js
@@ -5,3 +5,13 @@
 // @description For a tidier Hacker News
 // @version 0.0.1
 // ==/UserScript==
+
+var css = '<style type="text/css">'
++ 'body { margin:0; } '
++ 'body > center > table > tbody > tr:first-child > td { padding:5px 0 }'
++ 'body > center > table > tbody > tr:first-child > td > table { padding:0 5px !important }'
++ 'body > center > table > tbody > tr:first-child > td td:nth-child(2) img { width:30px !important }'
++ '</style>';
+
+var head = document.getElementsByTagName('head')[0];
+head.innerHTML += css;
