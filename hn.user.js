1e089216478b50ae18b03a89d0b146077e0ace01
Ralph Saunders <e@ralphsaunders.co.uk>
2012-05-14 15:27:42 +0100

    Metadata

---
+++ hn.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name Hacked News
+// @match https://news.ycombinator.com/*
+// @match http://news.ycombinator.com/*
+// @description For a tidier Hacker News
+// @version 0.0.1
+// ==/UserScript==
